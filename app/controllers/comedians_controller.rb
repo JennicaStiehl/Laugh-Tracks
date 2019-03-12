75de5875376877188888bfea5c76f168a8aaca0a
JennicaStiehl <jennica.stiehl@gmail.com>
2019-03-12 16:43:28 -0600

    adds user story 8

--- app/controllers/comedians_controller.rb
+++ app/controllers/comedians_controller.rb
@@ -1,14 +1,36 @@
 class ComediansController < ApplicationController
   def index
-    @comedians = Comedian.where(comedian_params)
+    if query_params
+      @comedians = Comedian.where(query_params)
+    else
+      @comedians = Comedian.all
+    end
     @avg_age = @comedians.avg_age
-    @specials = Special.where(comedian_params[:id])
+    @specials = Special.where(query_params[:id])
     @avg_length = @specials.avg_length
     @city_list = @comedians.city_list
   end
 
+  def new
+    @comedian = Comedian.new
+  end
+
+  def create
+    @comedian = Comedian.create(comedian_params)
+    if @comedian.save
+      flash[:success] = "Success!"
+      redirect_to comedians_path
+    else
+      redirect_to new_comedian_path
+    end
+  end
+
   private
-  def comedian_params
+  def query_params
     params.permit(:name, :age, :city)
+  end
+
+  def comedian_params
+    params.require(:comedian).permit(:name, :age, :city)
   end
 end
---
+++ spec/features/comedians/new_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "News", type: :feature do
+  before :each do
+    @c1 = Comedian.create(name: "John Mulaney", age: 20, city: "New York")
+    @c2 = Comedian.create(name: "Hari Kondabolu", age: 55, city: "New York")
+    @c3 = Comedian.create(name: "W. Kamau Bell", age: 45, city: "Palo Alto")
+    @c4 = Comedian.create(name: "W. Bell", age: 34, city: "Palo Alto")
+    @c5 = Comedian.create(name: "M. Bell", age: 34, city: "Palo Alto")
+    @sp1 = Special.create(name: "Kid Gorgeous", length: 60, image: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Ftimedotcom.files.wordpress.com%2F2018%2F11%2Fjohn-mulaney-kid-gorgeous-radio-city-top-10-comedy-best-of-culture-2018.jpg&w=1600&c=sc&poi=face&q=70", comedian: @c1)
+    @sp2 = Special.create(name: "New in Town", length: 60, image: "http://www.slate.com/content/dam/slate/articles/arts/interrogation/2012/02/120202_INTERROGATION_johnMulaney.jpg.CROP.rectangle3-large.jpg", comedian: @c1)
+    @sp3 = Special.create(name: "Warn Your Relatives", length: 60, image: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Ftimedotcom.files.wordpress.com%2F2018%2F11%2Fhari-kondabolu-warn-your-relatives-top-10-comedy-best-of-culture-2018.jpg&w=1600&c=sc&poi=face&q=70", comedian: @c2)
+    @sp4 = Special.create(name: "Private School Negro", length: 60, image: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Ftimedotcom.files.wordpress.com%2F2018%2F11%2Fw-kamau-bell-private-school-negro-top-10-comedy-best-of-culture-2018.jpg&w=1600&c=sc&poi=face&q=70", comedian: @c3)
+    @sp5 = Special.create(name: "Totally Biased", length: 60, image: "https://media.gettyimages.com/photos/comedian-w-kamau-bell-performs-onstage-at-totally-biased-with-w-kamau-picture-id163442351?s=612x612", comedian: @c3)
+  end
+  describe 'i see a way to a new comedian' do
+    it 'can create a new comedian' do
+      visit new_comedian_path
+      fill_in('Name', :with => "Jim Barton")
+      fill_in('Age', :with => 34)
+      fill_in('City', :with => 'New Orleans')
+      click_on 'Create Comedian'
+
+      expect(current_path).to eq(comedians_path)
+      expect(page).to have_content("Jim Barton")
+    end
+  end
+end
