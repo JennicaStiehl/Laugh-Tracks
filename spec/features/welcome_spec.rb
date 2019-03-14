188937292bd5092d569a21eef2612eb9ec14d5e2
JennicaStiehl <jennica.stiehl@gmail.com>
2019-03-13 20:51:11 -0600

    adds styling

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe "as a visitor" do
+  describe "when I navigate to the welcome page" do
+    it 'shows me links to all Comedians' do
+      visit welcome_path
+      expect(page).to have_content("All Comedians")
+    end
+  end
+end
